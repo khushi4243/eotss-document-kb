1592280b751216b141401085bad30d760a8258db
khushi4243 <khushi108@icloud.com>
2025-04-22 12:21:53 -0400

    constants changes

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -1,4 +1,4 @@
 export declare const AUTHENTICATION = true;
-export declare const cognitoDomainName = "eval-auth";
+export declare const cognitoDomainName = "eotss-doc-auth";
 export declare const OIDCIntegrationName = "";
-export declare const stackName = "EvalStack";
+export declare const stackName = "EOTSS-Doc-Stack";
